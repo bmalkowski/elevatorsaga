bc432d8542274e1eedca10c17f02c652b68419b5
Brian <bmalkowski@gmail.com>
2015-01-23 13:13:18 -0600

    Very basic logic

--- elevator.js
+++ elevator.js
@@ -1,32 +1,40 @@
 {
     init: function(elevators, floors) {
-      _.each(floors, function(f) {
-          f.on("down_button_pressed", function(event) { 
-              
-          });
+        var requests = new Array();
+        
+        _.each(floors, function(f) {
+            f.on("down_button_pressed", function(event) { 
+                requests.push(f.floorNum());
+            });
           
-          f.on("up_button_pressed", function(event) { 
-              
-          });
-      });
-      
-      _.each(elevators, function(e) {
-        e.on("idle", function() {
-            e.goToFloor(0);            
-        });      
-        
-        e.on("floor_button_pressed", function(floorNum) { 
-            e.goToFloor(floorNum);
-        });        
-      
-        e.on("passing_floor", function(floorNum, direction) { 
-            
+            f.on("up_button_pressed", function(event) { 
+                requests.push(f.floorNum());
+            });
         });
-        
-        e.on("stopped_at_floor", function(floorNum) {
+      
+        _.each(elevators, function(e) {
+            e.on("idle", function() {
+                if (_.isEmpty(requests)) {
+                    e.goToFloor(0);            
+                } else {
+                    e.goToFloor(requests.pop());
+                }
+            });      
             
-        });        
-      });
+            e.on("floor_button_pressed", function(floorNum) { 
+                e.goToFloor(floorNum);
+            });        
+            
+            e.on("passing_floor", function(floorNum, direction) { 
+                if (_.contains(requests, floorNum)) {
+                    e.stop();
+                }
+            });
+            
+            e.on("stopped_at_floor", function(floorNum) {
+                _.without(requests, _.findWhere(requests, floorNum));
+            });        
+        });
     },
     
     update: function(dt, elevators, floors) {
